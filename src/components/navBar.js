75b2879e6bc1de0d80c447347dcd6941678a0f91
vipisanan <vipisanan1@gmail.com>
2019-11-04 21:55:09 +0530

    create 2 component

---
+++ src/components/navBar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class navBar extends Component {
+render() {
+    return(
+        <div>
+            navBar
+        </div>
+    )
+}
+}
+export default navBar;
